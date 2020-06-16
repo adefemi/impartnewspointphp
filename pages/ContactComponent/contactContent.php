0deaedf8ced3c373ec244670930d19469ac93a99
Adefemi <adefemi.oseni@algorimsng.com>
2020-06-16 09:50:38 +0100

    updated

--- pages/ContactComponent/contactContent.php
+++ pages/ContactComponent/contactContent.php
@@ -7,12 +7,12 @@
         You can reach Impartnewspoint through;
         <br>
         <br>
-        Email: <a href="mail:wasiurauf15@gmail.com">wasiurauf15@gmail.com</a> / <a href="mail:ambassadorwasiu@yahoo.com">ambassadorwasiu@yahoo.com</a>
+        Email: <a href="mail:impartnewspoint@gmail.com">impartnewspoint@gmail.com</a>
         <br>
         <br>
         CALL: +234 805 688 5781 / +234 906 066 5239
         <br>
         <br>
-        We assure you,that your privacy is secured and guaranteed.
+        Your privacy will be highly protected and Secured.
     </p>
 </div>
