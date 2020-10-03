6fcd908472430b348edd472427976c8d5868acbe
Adefemi Oseni <adefemi.oseni@algorismng.com>
2020-10-03 18:17:03 +0100

    updated

--- components/layout_template.php
+++ components/layout_template.php
@@ -3,19 +3,21 @@ $request = $_SERVER['REQUEST_URI'];
 ?>
 <div class="blog-container">
     <div class="navbar">
-        <div class="brand"><a href="/"><img src="/assets/images/imaprt.png" alt=""/> <div class="title">
-                    <span>IMPARTNEWSPOINT</span>
-                    <small>Castle of Precise News</small>
-                </div></a></div>
+        <div class="brand"><a href="/"><img src="/assets/images/imaprt.png" alt="" />
+                <div class="title">
+                    <span class="brand-name">IMPARTNEWSPOINT</span>
+                    <small>Citadel Of Authentic Information</small>
+                </div>
+            </a></div>
 
         <div class="navRight desktop">
             <div class="top">
                 <div class="navLinks">
-                    <a href="http://www.facebook.com">Facebook</a>
+                    <a href="https://www.facebook.com/Impart-News-Point-100107794853454" target="_blank">Facebook</a>
                 </div>
 
                 <div class="navLinks">
-                    <a href="#">Twitter</a>
+                    <a href="https://twitter.com/impartnewspoint" target="_blank">Twitter</a>
                 </div>
                 <div class="navLinks">
                     <a href="#">LinkedIn</a>
@@ -52,26 +54,25 @@ $request = $_SERVER['REQUEST_URI'];
         <?php
         $similar_blog = "0";
         switch ($request) {
-            case '/' :
+            case '/':
                 require_once "pages/HomeComponents/homeContent.php";
                 break;
-            case '/about' :
+            case '/about':
                 require_once "pages/AboutComponent/aboutContent.php";
                 break;
-            case '/contact' :
+            case '/contact':
                 require_once "pages/ContactComponent/contactContent.php";
                 break;
             default:
-                if(preg_match_all('/\/[a-zA-Z0-9]+/', $request)){
+                if (preg_match_all('/\/[a-zA-Z0-9]+/', $request)) {
                     $similar_blog = "1";
                     require_once "pages/SingleBlogComponents/singleBlogContent.php";
                     break;
                 }
-                if(preg_match_all('/\/?[a-zA-Z0-9]+/', $request)){
+                if (preg_match_all('/\/?[a-zA-Z0-9]+/', $request)) {
                     require_once "pages/HomeComponents/homeContent.php";
                     break;
-                }
-                else{
+                } else {
                     require_once "pages/404Components/404Content.php";
                     break;
                 }
@@ -84,23 +85,23 @@ $request = $_SERVER['REQUEST_URI'];
     Impartnewspoint 2020 &copy; All Rights Reserved
 </footer>
 <script>
-    axios.get(BLOG_TAGS_URL).then(
-        res => {
-            const menu = document.getElementsByClassName("menu")[0];
-            let menuItems = "";
-            for(let a of res.data.results){
-                menuItems += `<a href="/?search=${a.title.toLowerCase()}">${a.title}</a>`
-            }
-            menu.innerHTML = menuItems;
+axios.get(BLOG_TAGS_URL).then(
+    res => {
+        const menu = document.getElementsByClassName("menu")[0];
+        let menuItems = "";
+        for (let a of res.data.results) {
+            menuItems += `<a href="/?search=${a.title.toLowerCase()}">${a.title}</a>`
         }
-    )
-    function toggleMenu() {
-        const menuLink = document.getElementById("menuLink")
-        if(menuLink.style.display === "block"){
-            menuLink.style.display = "none"
-        }
-        else {
-            menuLink.style.display = "block"
-        }
+        menu.innerHTML = menuItems;
     }
+)
+
+function toggleMenu() {
+    const menuLink = document.getElementById("menuLink")
+    if (menuLink.style.display === "block") {
+        menuLink.style.display = "none"
+    } else {
+        menuLink.style.display = "block"
+    }
+}
 </script>
