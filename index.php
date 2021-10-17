73c489782a693288c477f824067f9e0755a9446e
Adefemi Oseni <adefemigreat1995@gmail.com>
2021-10-17 10:58:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1927272486525742"
+     crossorigin="anonymous"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
@@ -12,7 +15,6 @@ const SIMILAR_BLOG_URL = baseUrl + "similar-blogs/";
 const BLOG_COMMENT_URL = baseUrl + "blog-comments/";
 </script>
 <script src="assets/scripts/utils.js"></script>
-</script>
 
 <?php
 $GLOBALS["base_api"] = "https://api.impartnewspoint.com/";
