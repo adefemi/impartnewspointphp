91d9ad122783f1755c133ac623f465cbdf916f8f
Adefemi Oseni <adefemigreat1995@gmail.com>
2021-10-18 10:18:25 +0100

    Update singleBlog.php

--- pages/singleBlog.php
+++ pages/singleBlog.php
@@ -13,6 +13,8 @@
 >
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1927272486525742"
+          crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/assets/styles/singleBlog.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/assets/images/imaprt.png"/>
